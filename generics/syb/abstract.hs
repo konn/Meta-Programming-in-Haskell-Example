65acc2e72cd159f55a500a1e9e2e90e13b4ac244
Hiromi Ishii <mr_konn@jcom.home.ne.jp>
2010-11-29 22:51:55 +0900

    Changed syb/abstract.hs to traverse Expr & Decl

--- generics/syb/abstract.hs
+++ generics/syb/abstract.hs
@@ -2,11 +2,16 @@
 import Data.Generics
 
 data Expr = Num Int
-          | Var String
-          | Plus Expr Expr
+          | Val String
+          | Let Decl Expr
+          | Plus  Expr Expr
           | Minus Expr Expr
           | Multi Expr Expr
           | Div   Expr Expr
+            deriving (Show, Eq, Data, Typeable)
+
+data Decl = String := Expr
+          | Seq Decl Decl
             deriving (Show, Eq, Data, Typeable)
 
 normalize' (Plus (Num n) (Num m))  = Num (n + m)
@@ -15,14 +20,22 @@ normalize' (Minus (Num n) (Num m)) = Num(n - m)
 normalize' (Div (Num n) (Num m))   = Num(n `div` m)
 normalize' x                       = x
 
-normalize :: Expr -> Expr
+-- | Can be used for any type including Expr
+normalize :: Data a => a -> a
 normalize = everywhere (mkT normalize')
 
--- 2 * (3 + 2 * 5) = 26
-tree1 = Multi (Num 2) (Plus (Num 3) (Multi (Num 2) (Num 5)))
+-- 2 * (3 + 2 * 5) == 26
+expr1 = Multi (Num 2) (Plus (Num 3) (Multi (Num 2) (Num 5)))
 
--- a * (3 + 2 * 5) = a * 13
-tree2 = Multi (Var "a") (Plus (Num 3) (Multi (Num 2) (Num 5)))
+-- a * (3 + 2 * 5) == a * 13
+expr2 = Multi (Val "a") (Plus (Num 3) (Multi (Num 2) (Num 5)))
+
+-- let a := 2 * 5 in 3 + 2 == let a := 10 in 5
+expr3 = Let ("a" := Multi (Num 2) (Num 5)) (Plus (Num 3) (Num 2))
+
+-- let a := 3 * 4 == let a := 12
+decl1 = "a" := Multi (Num 3) (Num 4)
+
 
 badNormalize :: Expr -> Expr
 badNormalize = everywhere' (mkT normalize')
